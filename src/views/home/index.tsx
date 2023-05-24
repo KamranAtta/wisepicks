60cc2b11a47fab0d3c13bec297f103faf16302da
Adil_cheema_redbuffer <adil.sharifcheema@redbuffer.net>
2023-05-24 15:21:48 +0500

    Added heading in index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <React.Fragment>
-      <div>Resource Ally</div>
+      <div>Resource Ally Heading</div>
     </React.Fragment>
   );
 }
