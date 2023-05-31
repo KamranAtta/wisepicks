c63c43b291abc86dc705e522a5a550823517d688
nabeelnoor <nabeelnoor914@gmail.com>
2023-05-31 11:28:40 +0500

    add loader base component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { ConfigProvider } from 'antd';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
+import Router from './Routes';
 import { theme } from './theme';
-import  Router  from './Routes'
 import { store } from './store/store';
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
     <ConfigProvider theme={theme}>
       <Provider store={store}>
         <BrowserRouter>
-        <Router />
+          <Router />
         </BrowserRouter>
-        </Provider>
+      </Provider>
     </ConfigProvider>
   );
 }
