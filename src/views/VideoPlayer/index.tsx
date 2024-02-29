3949c558cf108cbd6d993d263104d7617ac5c32f
kamranatta <kamran.creativetech@gmail.com>
2024-03-01 03:17:28 +0500

    autoplay video

--- src/views/VideoPlayer/index.tsx
+++ src/views/VideoPlayer/index.tsx
@@ -3,21 +3,20 @@ import YouTube, { YouTubeProps } from 'react-youtube';
 import { useMediaQuery } from '../../hooks/MediaQuery.hook';
 
 const VideoPlayer = ({ videoId }: any) => {
-    const matches = useMediaQuery('(min-width: 830px)');
-    const onPlayerReady: YouTubeProps['onReady'] = (event: any) => {
-        // access to player in all event handlers via event.target
-        event.target.pauseVideo();
-      }
-    
-      const opts: YouTubeProps['opts'] = {
-        height: matches ? '500': '200',
-        width: '100%',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 0,
-        },
-      };
-      return <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} /> 
+  const matches = useMediaQuery('(min-width: 830px)');
+  const onPlayerReady: YouTubeProps['onReady'] = (event: any) => {
+      event.target.playVideo();
+  };
+  
+  const opts: YouTubeProps['opts'] = {
+      height: matches ? '500': '200',
+      width: '100%',
+      playerVars: {
+          autoplay: 1,
+      },
+  };
+  
+  return <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} /> 
 };
 
 export default VideoPlayer;
